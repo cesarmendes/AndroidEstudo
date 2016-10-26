43e32aec92fef1346d3d2a8ab86104931f0d74e5
Cesar Mendes da Silva <cesar.mendes.silva@gmail.com>
2016-10-25 23:05:06 -0200

    initial commit

--- app/src/main/java/mendes/com/br/androidestudo/LoginActivity.java
+++ app/src/main/java/mendes/com/br/androidestudo/LoginActivity.java
@@ -1,7 +1,10 @@
 package mendes.com.br.androidestudo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        Button btn_login = (Button)findViewById(R.id.btn_login);
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/mendes/com/br/androidestudo/MainActivity.java
@@ -0,0 +1,53 @@
+package mendes.com.br.androidestudo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.mnu_option:
+                Toast.makeText(MainActivity.this, "Opções", Toast.LENGTH_SHORT).show();
+
+                break;
+
+            case R.id.mnu_exit:
+                Toast.makeText(MainActivity.this, "Sair", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+
+}
